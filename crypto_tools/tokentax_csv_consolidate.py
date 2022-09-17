83cf11ba068014246ec5b157351cfde5e6e6ab3e
James Lewellyn <Lewellyn.James@gmail.com>
2022-09-17 16:30:04 -0500

    almost

--- crypto_tools/tokentax_csv_consolidate.py
+++ crypto_tools/tokentax_csv_consolidate.py
@@ -1092,7 +1092,7 @@ def main() -> None:
                 transaction_hash,
             )
             quick_print_transactions_same_hash_list(separated_transaction_list)
-            break
+            # break
         # logger.info("__________________________________________________")
 
     with output_tokentax_csv_file_path.open("w") as output_tokentax_csv_file:
